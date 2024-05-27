66aade1bb2cdafc90e23b1a291637061fd13a568
Adam Owolabi <113387571+AdamOwolabi@users.noreply.github.com>
2024-05-27 09:41:49 -0400

    Add files via upload

---
+++ string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Input: ");
+    printf("Output: ");
+    int length = strlen(s);
+
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("%c", s[i]);
+    }
+
+    printf("\n");
+}
