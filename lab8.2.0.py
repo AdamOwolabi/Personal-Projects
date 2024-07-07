284da5441cc800fc4242927254a80582cc78ede6
Adam Owolabi <113387571+AdamOwolabi@users.noreply.github.com>
2024-07-06 20:12:38 -0400

    Add files via upload

---
+++ lab8.2.0.py
@@ -0,0 +1,13 @@
+#ADAM OWOLABI
+#PROGRAM TO CONVERT THE TITLE TO CAPITAL LETTER
+def main():
+    #prompt user for input
+    
+    x = str(input("Enter a sentence to be capitalized: "))
+    y = x.title()
+    print(y)
+
+
+
+main()
+
